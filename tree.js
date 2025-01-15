34b5519aff2895057193bab0a0f70ad2508697ff
Alina181 <132784042+Alina181@users.noreply.github.com>
2025-01-15 17:03:47 +0400

    fix

--- tree.js
+++ tree.js
@@ -3,6 +3,19 @@ linkStylesheet.rel = 'stylesheet';
 linkStylesheet.href = 'tree.css';
 document.head.appendChild(linkStylesheet);
 
+// Создаем новый элемент div
+const div = document.createElement('div');
+
+// Устанавливаем атрибут id
+div.id = 'question-mark';
+
+// Устанавливаем текстовое содержимое
+div.textContent = '?';
+
+// Добавляем div в body или другой контейнер на странице
+document.body.appendChild(div);
+
+
 const linkIcon = document.createElement('link');
 linkIcon.rel = 'icon';
 linkIcon.type = 'image/x-icon';
